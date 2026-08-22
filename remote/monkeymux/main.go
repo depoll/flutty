4ff3d792154ab0325b6a0f66992db4bf2f37ff4f
David Poll <depoll@mac-mini-pro.home>
2026-08-22 08:53:06 -0700

    Restore compacted Pi context on native resume

--- remote/monkeymux/main.go
+++ remote/monkeymux/main.go
@@ -59,7 +59,7 @@ type muxProcess interface {
 }
 
 const (
-	monkeyMuxVersion                  = "0.1.171"
+	monkeyMuxVersion                  = "0.1.172"
 	defaultColumns                    = 80
 	defaultRows                       = 24
 	maxTitleBytes                     = 160
